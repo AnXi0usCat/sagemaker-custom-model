a426dd89d061b34a276bb5e6f071a0299896d2e1
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2019-12-01 21:49:53 +0000

    inital commit

---
+++ src/train/lightgbm.py
@@ -0,0 +1,12 @@
+from rest_api import serve
+
+def serve():
+    return serve()
+
+
+def meow():
+    print("meow meow cats")
+    
+    
+if __name__ == '__main__':
+    meow()
---
+++ src/train/rest_api.py
@@ -0,0 +1,3 @@
+
+def serve():
+    print('sdcfghnrfvbhtgfdgbf')
